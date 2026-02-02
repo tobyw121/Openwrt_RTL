f881ab6e98dc5de2ca517aa22f69cb4f3927bd70
Toby <tobiasw763@gmail.com>
2026-02-02 20:33:13 +0100

    Upload Batch 4879

---
+++ GPL_ MR62X/sdk/openwrt-21.02/target/linux/linux-4.4/drivers/net/wireless/realtek/g6_wifi_driver/phl/hal_g6/mac/fw_ax/inc_hdr/hal1115_spec.h
@@ -0,0 +1,16 @@
+#ifndef __HAL1115_SPEC_H__
+#define __HAL1115_SPEC_H__
+
+/*
+This header will be shared to halmac
+*/
+
+
+//Macid number
+#define SPEC_MAX_MACID_NUM_NIC 32   //NIC or Software AP 
+#define SPEC_MAX_MACID_NUM_AP 128  //AP
+
+
+
+#endif  //__HAL1115_SPEC_H__
+
