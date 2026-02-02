07541b09992cd10f3e31eb91299dbcb4e5ee9a3b
Toby <tobiasw763@gmail.com>
2026-02-02 12:14:27 +0100

    Upload Batch 66

---
+++ GPL_AX12v1/rtl8197/backports-5.2.8-1/drivers/net/wireless/realtek/rtl8192fe/git_info2.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2021 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * The full GNU General Public License is included in this distribution in the
+ * file called LICENSE.
+ *
+ * Contact Information:
+ * wlanfae <wlanfae@realtek.com>
+ * Realtek Corporation, No. 2, Innovation Road II, Hsinchu Science Park,
+ * Hsinchu 300, Taiwan.
+ *
+ * Larry Finger <Larry.Finger@lwfinger.net>
+ *
+ *****************************************************************************/
+#ifndef __GIT_INFO2_H__
+#define __GIT_INFO2_H__
+ /*@--------------------------[Define] ---------------------------------------*/
+
+#define RTK_MAIN_SHA1   "98fa674fffac18322deeb75d878401e84df07362"
+#define RTK_MAIN_TAGINFO     "v3.4.13-1292-g98fa674f"
+
+#define RTK_CMN_INFO_SHA1   "7acb9d532d95a504ef63099e563c14fc57e8a88c"
+#define RTK_CMN_INFO_TAGINFO     "v3.4.13-4-g7acb9d5"
+
+#define RTK_PHYDM_SHA1   "5a1ce4805d4618b63a3927ce36386f79c81defab"
+#define RTK_PHYDM_TAGINFO     "v3.4.13-362-g5a1ce480"
+
+#define RTK_PHYDM_HALRF_SHA1   "90315b08db3cc5db6d95fc353e82ffde09fa3fa8"
+#define RTK_PHYDM_HALRF_TAGINFO     "v3.4.13-282-g90315b0"
+
+#endif
