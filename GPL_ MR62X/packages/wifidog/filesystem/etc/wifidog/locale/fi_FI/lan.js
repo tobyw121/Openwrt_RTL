22ae3c02d1a43824ce625d2e05f9a40df72b95a1
Toby <tobiasw763@gmail.com>
2026-02-02 23:48:54 +0100

    Upload Batch 5909

---
+++ GPL_ MR62X/packages/wifidog/filesystem/etc/wifidog/locale/fi_FI/lan.js
@@ -0,0 +1,20 @@
+(function ($) {
+    $.su = $.su || {};
+    $.su.CHAR = {
+        PROTAL: {
+            SUCCESS: 'Onnistui',
+            FAILED: 'Kirjautuminen ei onnistu. Yritä myöhemmin uudelleen.',
+            WELCOME: 'Tervetuloa',
+            PASSWORD: 'Salasana',
+            LOGIN: 'Kirjaudu sisään',
+            TERMS_OF_USE: 'käyttöehdot',
+            ACCEPT_TERMS_OF_USE: 'Olen lukenut ja hyväksynyt <a class="link" href="#" target="_blank">käyttöehdot</a>',
+            TERMS_OF_USE_ERROR: 'Lue ja valitse "Hyväksyn käyttöehdot".'
+        },
+        ERROR: {
+            '00000001': 'Tämä kenttä on pakollinen.',
+            '00000002': 'Virheellinen muoto.',
+            '00000003': 'Virheellinen salasana.'
+        }
+    };
+}(jQuery));
