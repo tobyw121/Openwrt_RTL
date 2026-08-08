4573a22a886fba4da5ad9c838176583934f666a2
tobyw121 <tobiasw763@gmail.com>
2026-08-08 06:30:56 +0200

    Initial Upload Batch 7

---
+++ msdk-4.3.6-mips-EB-2.6.32-0.9.30.3-m32-120424.tar.bz2_extracted/msdk-4.3.6-mips-EB-2.6.32-0.9.30.3-m32-120424/include/linux/raid/..install.cmd
@@ -0,0 +1,1 @@
+cmd_/tmp/include/linux/raid/.install := perl scripts/headers_install.pl /opt/cadpc4/tonywu/repo/rlxlinux-2.6.32/linux-2.6.32/include/linux/raid /tmp/include/linux/raid mips md_p.h md_u.h; perl scripts/headers_install.pl /opt/cadpc4/tonywu/repo/rlxlinux-2.6.32/linux-2.6.32/include/linux/raid /tmp/include/linux/raid mips ; touch /tmp/include/linux/raid/.install
