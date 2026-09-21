41c71cc988d27f9c288e2a33952af2a713f5cc87
tobyw121 <tobiasw763@gmail.com>
2026-09-22 00:31:46 +0200

    Initial Upload Batch 242

---
+++ package/kernel/rtl8192cd-rtl8197f/src/git_info2.h
@@ -0,0 +1,11 @@
+#ifndef _MW5_GIT_INFO2_H
+#define _MW5_GIT_INFO2_H
+#define RTK_MAIN_SHA1 "mw5-v40-port"
+#define RTK_MAIN_TAGINFO "Linux-6.6 compatibility"
+#define RTK_CMN_INFO_SHA1 "mw5-v40-port"
+#define RTK_CMN_INFO_TAGINFO "Linux-6.6 compatibility"
+#define RTK_PHYDM_SHA1 "mw5-stock-kernel-extracted"
+#define RTK_PHYDM_TAGINFO "RTL8197F/RTL8822B"
+#define RTK_PHYDM_HALRF_SHA1 "mw5-stock-kernel-extracted"
+#define RTK_PHYDM_HALRF_TAGINFO "RFE5/table bundle"
+#endif
